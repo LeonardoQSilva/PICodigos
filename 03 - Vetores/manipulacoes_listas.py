fffe918a2225304d90ff3bbeee4f24028caf370d
LeonardoQSilva <ll.q.silva@hotmail.com>
2026-03-30 21:58:47 -0300

    Add files via upload

---
+++ 03 - Vetores/manipulacoes_listas.py
@@ -0,0 +1,55 @@
+def inverter_lista( Lista: [int] ) -> [int]:
+    R = list()
+    # Retorna a Lista com ordem invertida.
+    # Usar laços para gerar a nova lista invertida.
+    # Exemplo:
+    # Entrada: [1, 2, 3, 4]
+    # Saída: [4, 3, 2, 1]
+    return R
+
+def eliminar_elemento( x: int, Lista: [int]) -> [int]:
+    R = list()
+    # Retorna uma lista removendo todos os elementos de valor x (se houver).
+    # Usar laços para remover.
+    # Exemplos:
+    # Entrada: x = 2 e Lista = [1, 2, 3, 4]
+    # Saída: [1, 3, 4]
+    # Entrada: x = 5 e Lista = [1, 2, 3, 4]
+    # Saída: [1, 2, 3, 4]
+    return R
+
+def menor_elemento( Lista: [int] ) -> int:
+    # Retorna o menor valor de uma lista.
+    # Considere que a lista não tem valores repetidos.
+    # Usar laços para buscar o menor valor.
+    # Exemplo:
+    # Entrada: [6, 4, 2, 5, 3]
+    # Saída: 2
+    return 0
+
+def segundo_menor_elemento( Lista: [int] ) -> int:
+    # Retorna o segundo menor valor de uma lista.
+    # Considere que a lista não tem valores repetidos.
+    # Dica: você pode utilizar as duas funções acima!
+    # Exemplos:
+    # Entrada: [6, 2, 4, 3, 5]
+    # Saída: 3
+    return 0
+
+def agrupar_elementos( quantidade: int, Lista: [int]) -> [int]:
+    R = list()
+    # Retorna uma lista que agrupa em uma quantidade definida somando os valores.
+    # Exemplos:
+    # Entrada: quantidade = 2 e Lista = [1, 2, 3, 4, 5, 6]
+    # Saída: [3, 7, 11]
+    # Explicação: [1+2=3, 3+4=7, 5+6=11]
+    # Entrada: quantidade = 3 e Lista = [1, 2, 3, 4, 5, 6]
+    # Saída: [6, 15]
+    # Explicação: [1+2+3=6, 4+5+6=15]
+    # Entrada: quantidade = 2 e Lista = [1, 2, 3, 4, 5]
+    # Saída: [3, 7, 5]
+    # Explicação: [1+2=3, 3+4=7, 5=5]
+    # Entrada: quantidade = 3 e Lista = [1, 2, 3, 4, 5]
+    # Saída: [6, 9]
+    # Explicação: [1+2+3=6, 4+5=9]
+    return R
---
+++ 03 - Vetores/media_acima_da_media.py
@@ -0,0 +1,10 @@
+
+tamanho = int(input())
+
+# Leia todas as notas (quantidade definido em tamanho);
+
+# Calcule a média de todas as notas;
+
+# Calcule uma nova média considerando apenas as notas acima da média. Desconsiderar as notas iguais a média;
+
+# Imprima a média acima da média.
