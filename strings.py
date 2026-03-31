3f793259ff57022e4508405b7db975bda2b9b058
LeonardoQSilva <ll.q.silva@hotmail.com>
2026-03-30 21:38:25 -0300

    Adicionando Atividade Strings

---
+++ strings.py
@@ -0,0 +1,36 @@
+def inverte_caracteres(frase):
+    # Retorna a palavra/frase invertida.
+    # Dica: Faça um laço decrescente copiando o texto original para uma string.
+    # Exemplo 1:
+    # Entrada: ABCDEF
+    # Saída: FEDCBA
+    # Exemplo 2:
+    # Entrada: Universidade Federal do ABC
+    # Saída: CBA od laredeF edadisrevinU
+    return ""
+
+def inverte_caracteres_em_palavras(frase):
+    # Retorna a frase apenas com as palavras invertidas.
+    # A ordem das palavras são preservadas.
+    # Dica: faça um laço até o fim da palavra (identifcado por espaço ou final).
+    # Utilize esta inforamção para extrair e inverter a palavra.
+    # Exemplo 1:
+    # Entrada: ABC DEF GHI
+    # Saída: CBA FED IHG
+    # Exemplo 2:
+    # Entrada: Arara voa leve
+    # Saída: ararA aov evel
+    return ""
+
+def inverte_palavras(frase):
+    # Retorna as palavras invertidas, mas não os caracteres.
+    # Dica: faça um laço decrescente até o início da plavra.
+    # Utilize esta informação para copiar a palavra para um novo texto.
+    # Exemplo 1:
+    # Entrada: ABC DEF GHI
+    # Saída: GHI DEF ABC
+    # Exemplo 2:
+    # Entrada: Hoje não chove
+    # Saída: chove não Hoje
+    return ""
+
